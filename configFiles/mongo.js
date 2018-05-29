1436375c3b44779fc3e8cabd6787c8e761ca72a5
Divesh Sankhla <sankhlasaini@gmail.com>
2018-05-29 21:45:46 +0530

    mongo connected with atlas

--- configFiles/mongo.js
+++ configFiles/mongo.js
@@ -4,13 +4,13 @@ var config = require("./mongoConfig");
 
 function setupMongooseConnections() {
 
-  mongoose.connect(config.MONGO.mongoURL);
+  mongoose.connect(config.MONGO.mongoAtlasURL);
 
   mongoose.connection.on('connected', () => {
-    console.log('MongoDB is Connected to', config.MONGO.mongoURL);
+    console.log('MongoDB is Connected to', config.MONGO.mongoAtlasURL);
   });
 }
 
 module.exports = {
   setupMongooseConnections: setupMongooseConnections
-}
+};
--- configFiles/mongoConfig.js
+++ configFiles/mongoConfig.js
@@ -3,10 +3,13 @@ var mongo = {
   host: process.env.MONGO_HOST || '127.0.0.1',
   port: process.env.MONGO_PORT || 27017,
   masterDB: process.env.MONGO_DB || 'dvs',
-  mongoURL :''
+  mongoURL :'',
+  mongoAtlasURL :''
 };
 
 mongo.mongoURL = 'mongodb://' + mongo.host + ':' + mongo.port + '/' + mongo.masterDB;
+
+mongo.mongoAtlasURL = 'mongodb+srv://dvs:admin@dvs-db-lbr2z.mongodb.net/dvs';
 
 var config = {
   MONGO: mongo,
